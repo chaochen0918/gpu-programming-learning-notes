538fbc069db0e7f63f95c050101808f70f989d15
chaochen0918 <walton860918@gmail.com>
2026-03-24 04:30:20 +0000

    add comments explaining details of each triton function

--- naive_mm.py
+++ naive_mm.py
@@ -5,13 +5,13 @@ import torch
 
 @triton.jit
 def naive_matmul_kernel(
-    A_ptr, B_ptr, C_ptr,
+    A_ptr, B_ptr, C_ptr, # A_ptr is the base address — a raw memory address pointing to the first element A[0, 0] in the GPU's global memory address space.
     M, N, K,
     stride_am, stride_ak,
     stride_bk, stride_bn,
     stride_cm, stride_cn,
 ):
-    # Each program instance handles one output element C[m, n]
+    # Each program instance (block instance) handles one output element C[m, n]
     pid_m = tl.program_id(axis=0)
     pid_n = tl.program_id(axis=1)
 
@@ -22,9 +22,11 @@ def naive_matmul_kernel(
     # Accumulator for the dot product
     acc = tl.zeros((), dtype=tl.float32)
 
-    for k in range(K):
-        a = tl.load(A_ptr + pid_m * stride_am + k * stride_ak)
-        b = tl.load(B_ptr + k   * stride_bk  + pid_n * stride_bn)
+    for k in range(K): # do the dot product of A[pid_m, :] and B[:, pid_n]
+        # tl.load(A_ptr + offset) : Each Triton program instance (a GPU thread group) is issuing a memory instruction to read from GPU global memory into a register.
+        # offset = base + row * K + col * 1
+        a = tl.load(A_ptr + pid_m * stride_am + k * stride_ak) # A[offset]
+        b = tl.load(B_ptr + k   * stride_bk  + pid_n * stride_bn) # B[offset]
         acc += a * b
 
     tl.store(C_ptr + pid_m * stride_cm + pid_n * stride_cn, acc)
@@ -58,7 +60,7 @@ def naive_matmul(A: torch.Tensor, B: torch.Tensor) -> torch.Tensor:
 # --- Quick correctness check ---
 if __name__ == "__main__":
     torch.manual_seed(0)
-    A = torch.randn(64, 128, device="cuda", dtype=torch.float32)
+    A = torch.randn(64, 128, device="cuda", dtype=torch.float32) # This allocates a buffer in GPU DRAM (global memory) and fills it there.
     B = torch.randn(128, 32, device="cuda", dtype=torch.float32)
 
     C_triton = naive_matmul(A, B)
